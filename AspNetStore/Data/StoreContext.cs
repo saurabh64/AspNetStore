300abc1c1197c2696656b46791fa037752e94820
saurabh64 <saurabh.mayekar64@gmail.com>
2016-09-12 00:14:18 +0530

    Changes add and webconfig

---
+++ AspNetStore/Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AspNetStore.Data
+{
+    public class StoreContext: DbContext
+    {
+        public StoreContext():base("Default")
+        {
+
+        }
+    }
+}
