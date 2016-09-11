1f9ee8369b8c8182012ed253fc9880521b2c1b4e
saurabh64 <saurabh.mayekar64@gmail.com>
2016-09-11 22:33:45 +0530

    Add all files

---
+++ AspNetStore/Controllers/StoreController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public  async Task<ActionResult> Index() //for all products
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Browse(string id)  //for category
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> Details(int id) //for individual product
+        {
+            return View();
+        }
+    }
+}
